86c4306727a31af8b0563d955d3683a6e1594e09
Sarp Ozcetin <sarpozcetin@gmail.com>
2024-03-15 20:28:13 -0700

    Cleaned up code from previous commit

--- api.js
+++ api.js
@@ -1,12 +1,11 @@
 /*
 * To-Do
-* Find a way to find users latitude and longitude dynamically
 * Convert sunrise and sunset unix utc to local time javascript
-* http://www.geoplugin.net/json.gp?ip=24.68.121.168
 */
 
-/*fixed lat and long for sidney BC*/
 const getIP = `https://api.ipify.org`
+
+/*Converts UTC time to local time*/
 function UTC_convert(sunrise, sunset) {
     var localRise = new Date(sunrise *1000);
     var localSet = new Date(sunset * 1000);
@@ -27,7 +26,11 @@ fetch(getIP)
     })
 
     .then (response => {
-        return response.json();
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("API network response failed")
+        }
     })
 
     .then(location => {
@@ -35,41 +38,23 @@ fetch(getIP)
     })
 
     .then(response => {
-        return response.json();
-
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("API network response failed")
+        }
     })
 
     .then(weatherData => {
-        //console.log(weatherData.sys.sunrise)
-        //console.log(weatherData.sys.sunset)
         const [rise, set] = UTC_convert(weatherData.sys.sunrise, weatherData.sys.sunset)
         console.log(rise)
         console.log(set)
-
-
     })
 
     .catch (error => {
         console.error('Error with fetching the API', error);
     });
 
-//const b = 'http://www.geoplugin.net/json.gp?ip=${a}'
-//console.log(b)
-/*fetch(weatherAPI)
-    .then (response => {
-        if (!response.ok) {
-            throw new error('Network Response was unsuccessful');
-        }
-        return response.json();
-    })
-
-    .then (data => {
-        console.log(data)
-    })
-
-    .catch (error => {
-        console.error(error);
-    });*/
 
 
 
